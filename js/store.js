274baf146f30c5b22a6904028de4a9c8289493af
Amirali <amiali1382yaghobi@gmail.com>
2023-07-02 22:24:55 +0330

    shop-basket

--- js/store.js
+++ js/store.js
@@ -1,13 +1,18 @@
 let productData = [
-    {id:1,title:"Album1",price:200,image:'./Image/Album1.png'},
-    {id:2,title:"Album2",price:399,image:'./Image/Album2.png'},
-    {id:3,title:"Album3",price:300,image:'./Image/Album3.png'},
-    {id:4,title:"Album4",price:99,image:'./Image/Album4.png'},
-    {id:5,title:"Cofee",price:100,image:'./Image/Cofee.png'},
-    {id:6,title:"Shirt",price:299,image:'./Image/Shirt.png'},
+    { id: 1, title: 'Album 1', price: 12.93, img: 'Images/Album 1.png' },
+    { id: 2, title: 'Album 2', price: 21, img: 'Images/Album 2.png' },
+    { id: 3, title: 'Album 3', price: 33, img: 'Images/Album 3.png' },
+    { id: 4, title: 'Album 4', price: 41.98, img: 'Images/Album 4.png' },
+    { id: 5, title: 'Coffee', price: 98, img: 'Images/Cofee.png' },
+    { id: 6, title: 'Shirt', price: 65.33, img: 'Images/Shirt.png' },
 ]
+let userbasket = []
+console.log(productData)
 let $ = document
+let productsList = $.querySelector('.shop-items')
+
 productData.forEach(function(product){
+
 let productContainer = $.createElement('div')
 productContainer.classList.add('shop-item')
 
@@ -15,6 +20,46 @@ let titleSpan = $.createElement('span')
 titleSpan.classList.add('shop-item-title')
 titleSpan.innerHTML = product.title
 
-let productImag = 
+let productImag = $.createElement('img')
+productImag.setAttribute('src',product.img)
+productImag.classList.add('shop-item-image')
+
+let detailContainer = $.createElement('div')
+detailContainer.classList.add('shop-item-details')
+
+let productPrice = $.createElement('span')
+productPrice.classList.add('shop-item-price')
+productPrice.innerHTML = product.price
+
+let addButton = $.createElement('button')
+addButton.className = "btn btn-primary shop-item-button"
+addButton.innerHTML = "ADD TO CARD"
+addButton.addEventListener('click',function(){
+    addProductToBasket(product.id)
+})
+detailContainer.append(productPrice,addButton)
+productContainer.append(titleSpan,productImag,detailContainer)
+productsList.append(productContainer)
+})
+
+
+
+function addProductToBasket(productId){
+   let userProduct = productData.find(function(product){
+    return product.id == productId
+    })
+   userbasket.push(userProduct)
+   console.log(userbasket)
+}
+
+function basketProductGenrator(basketArr){
+basketArr.forEach(function(product){
+
+let cartRow = $.createElement('div')
+cartRow.classList.add('cart-row')
+
+let cartItem = $.createElement('div')
+cartItem.classList.add('cart-item','cart-column')
 
 })
+}
